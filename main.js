e1d0ff42a731639c202df832b10c9acb7bca234e
Filipe Moreira Coelho <60034638+Filipe-m@users.noreply.github.com>
2022-06-27 14:15:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Mudar a Cor do BackGround
+function change() {
+  var x = document.getElementById("color").value;
+  document.getElementById("body").style.background = `linear-gradient(to left, ${x} , white)`;
+};
+
+
+
+
